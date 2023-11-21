68a37fa6ac04e54fb0439d1a0f1d16fe0791c02d
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2023-11-21 15:55:06 +0300

    Initial setup

---
+++ backend/src/main/java/com/aydakar/plus30backend/Plus30BackendApplication.java
@@ -0,0 +1,13 @@
+package com.aydakar.plus30backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plus30BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plus30BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aydakar/plus30backend/Plus30BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydakar.plus30backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plus30BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
