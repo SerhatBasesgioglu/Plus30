c502c89491507e7395045b1bb2e7872a2412466e
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2023-12-13 11:17:03 +0300

    add localstorage template for presets

--- frontend/src/components/LobbyForm.jsx
+++ frontend/src/components/LobbyForm.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import createLobby from "../services/createLobby";
 
 const LobbyForm = () => {
-  const [inputs, setInputs] = useState({
+  const defaultLobby = {
     lobbyname: "",
     lobbypassword: "",
     mapid: "12",
     teamsize: "1",
     spectatorpolicy: "AllAllowed",
-  });
+  };
+
+  const [inputs, setInputs] = useState(defaultLobby);
+
+  const [presets, setPresets] = useState([
+    defaultLobby,
+    defaultLobby,
+    defaultLobby,
+  ]);
+  const [presetIndex, setPresetIndex] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -20,6 +29,30 @@ const LobbyForm = () => {
     const value = event.target.value;
     setInputs((prevState) => ({ ...prevState, [name]: value }));
   };
+
+  const handleSave = () => {
+    const nextPresets = presets.map((c, i) => {
+      if (i === presetIndex) return inputs;
+      return c;
+    });
+    setPresets(nextPresets);
+    localStorage.setItem("presets", JSON.stringify(nextPresets));
+  };
+
+  const handleLoad = () => {
+    setInputs(presets[presetIndex]);
+  };
+
+  const handleClear = () => {
+    setInputs(defaultLobby);
+  };
+
+  /*
+  useEffect(() => {
+    const savedPresets = localStorage.getItem("presets");
+    if (savedPresets) setPresets(savedPresets);
+  }, []);
+*/
 
   return (
     <div className="col-md-3">
@@ -84,9 +117,65 @@ const LobbyForm = () => {
             <option value="FriendsAllowed">Friends</option>
             <option value="NotAllowed">Not</option>
           </select>
-          <input type="submit" className="btn btn-secondary" />
+          <div className="my-2">
+            <input type="submit" className="btn btn-secondary" />
+            <button
+              type="button"
+              className="btn btn-info mx-2"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className="btn btn-info mx-2"
+              onClick={handleLoad}
+            >
+              Load
+            </button>
+            <button
+              type="button"
+              className="btn btn-info mx-2"
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+          </div>
         </div>
       </form>
+      <div>
+        <p className="mt-4">Presets</p>
+        <button
+          className={`btn btn-info mx-2 ${
+            presetIndex === 0 ? "bg-primary" : "bg-secondary"
+          }`}
+          onClick={() => {
+            setPresetIndex(0);
+          }}
+        >
+          1
+        </button>
+        <button
+          className={`btn btn-info mx-2 ${
+            presetIndex === 1 ? "bg-primary" : "bg-secondary"
+          }`}
+          onClick={() => {
+            setPresetIndex(1);
+          }}
+        >
+          2
+        </button>
+        <button
+          className={`btn btn-info mx-2 ${
+            presetIndex === 2 ? "bg-primary" : "bg-secondary"
+          }`}
+          onClick={() => {
+            setPresetIndex(2);
+          }}
+        >
+          3
+        </button>
+      </div>
     </div>
   );
 };
