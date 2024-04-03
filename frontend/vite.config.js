1f836e2213f83d2e03ad3f1bf14ae43d9f5ccf84
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-04-03 22:34:01 +0300

    github actions setup, change react build path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  build: { outDir: "../electron/resources" },
+
   plugins: [react()],
   resolve: {
     alias: {
