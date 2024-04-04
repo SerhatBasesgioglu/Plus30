beb745b5faa8ae2c43c9d5b98e31d8ef81ab2741
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-04-04 12:22:05 +0300

    update build configs

--- autobuilder.sh
+++ autobuilder.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+#backend build
 cd backend
+./mvnw install
 ./mvnw clean package
+#frontend build
 cd ../frontend
+npm install
 npm run build
+#electron merge, build
 cd ..
 cp backend/target/*.jar electron/resources
 cp -R frontend/dist electron/resources
