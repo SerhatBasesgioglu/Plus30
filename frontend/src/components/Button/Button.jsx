fb8da72ed202117c8fcee9ff5f16f541d332bd51
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-04-06 11:19:48 +0300

    update toggle

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 const Button = ({ text, className, type, onClick }) => {
-  const defaultClasses = "bg-blue-600 p-1 m-1 min-w-8 h-6 text-xs rounded-md "; //Leave a blank at the end!!
+  const defaultClasses = "bg-blue-600 hover:bg-blue-700 p-1 m-1 min-w-8 h-6 text-xs rounded-md "; //Leave a blank at the end!!
   return (
     <button className={defaultClasses + className} type={type} onClick={onClick}>
       {text}
